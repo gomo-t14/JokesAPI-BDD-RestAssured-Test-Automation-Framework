a9c83aaadfeb016ee94045f0f67748c1e9516b54
gomo-t14 <gomotda14@gmail.com>
2026-03-02 19:33:46 +0100

    chore: set up runner and stepdifinition packages

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,16 @@
+package runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue     = {"com.example.stepdefinitions"},
+        plugin   = {
+                "pretty",
+                "html:target/cucumber-reports/report.html",
+                "json:target/cucumber-reports/cucumber.json"
+        },
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+}
